de9005f5c3cd2f3605c9e329e1d7498b48f6c299
Nick Rodriguez <endotnick@gmail.com>
2017-08-30 22:55:21 +0000

    add pangram

---
+++ ruby/pangram/pangram.rb
@@ -0,0 +1,12 @@
+module BookKeeping
+  VERSION = 5
+end
+
+class Pangram
+  def self.pangram? string
+    string = string.downcase
+    ('a'..'z').all? do |char| 
+      string.include?(char)
+    end
+  end
+end
