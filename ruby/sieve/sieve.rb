b4b48676ec4efff5a16815c8fc08df5c98a7e496
Nick Rodriguez <endotnick@gmail.com>
2017-09-04 19:15:29 +0000

    add sieve of Eratosthenes

---
+++ ruby/sieve/sieve.rb
@@ -0,0 +1,32 @@
+module BookKeeping
+  VERSION = 1
+end
+  
+class Sieve
+  def initialize(num)
+    @num = num
+  end
+  
+  def primes
+    primes = []
+    values = *(2..@num)
+    
+    # while values exist that could be prime
+    while values.size > 0 do
+      # select the next value
+      i = values.first
+      # this value must be prime at this point, so add it to primes
+      primes << i
+      # see if it is a divisor for any remaining values
+      values.each do |val|
+        # if it is
+        if (val % i == 0)
+          # remove that value from consideration
+          values.delete(val)
+        end
+      end
+    end
+    primes
+  end
+  
+end
