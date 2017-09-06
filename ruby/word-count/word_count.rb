1fcf238f649276b1dea45093f0ff5848f81d5456
Nick Rodriguez <endotnick@gmail.com>
2017-09-06 21:52:44 +0000

    add word-count.rb

---
+++ ruby/word-count/word_count.rb
@@ -0,0 +1,21 @@
+module BookKeeping
+  VERSION = 1
+end
+
+class Phrase
+  def initialize(phrase)
+    @phrase = phrase
+  end
+  
+  def word_count
+    counts = Hash.new(0)
+    # remove single quotes surrounding words
+    @phrase.gsub!(/'([^\s]+)'/, '\1')
+    
+    # split on all non alphanumeric characters except "'" and all whitespace.
+    @phrase.split(/[\W\s&&[^']]+/).each do |word| 
+      counts[word.downcase] += 1
+    end
+    counts
+  end
+end
