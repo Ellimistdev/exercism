c708922b98109b3a3d388d794710dc302fa56e23
Nick Rodriguez <endotnick@gmail.com>
2017-09-10 23:02:03 +0000

    cleanup bob.rb

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -6,19 +6,17 @@ class Bob
   def self.hey(str)
     # remove all whitespace
     str.gsub!(/\s/, "")
-    
-    reply = case
-      # this case must go before the str.upcase check, str.upcase on "" returns true. 
-      when str == ""
-        "Fine. Be that way!"
-      # TODO: find solution that is not true when str == ""
-      when str == str.upcase && !str.match(/^[^a-zA-Z]+$/)
-        "Whoa, chill out!"
-      when str[-1] == '?'
-        "Sure."
-      else
-        "Whatever."
+    case
+    # this case must go before the str.upcase check, str.upcase on "" returns true. 
+    when str == ""
+      "Fine. Be that way!"
+    # TODO: find solution that is not true when str == ""
+    when str == str.upcase && !str.match(/^[^a-zA-Z]+$/)
+      "Whoa, chill out!"
+    when str[-1] == '?'
+      "Sure."
+    else
+      "Whatever."
     end
-    reply
   end
 end
