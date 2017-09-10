331baecd4e774ce0292e039ae212c5262e6389da
Nick Rodriguez <endotnick@gmail.com>
2017-09-10 22:50:01 +0000

    add bob.rb solution

---
+++ ruby/bob/bob.rb
@@ -0,0 +1,24 @@
+module BookKeeping
+  VERSION = 1
+end
+
+class Bob
+  def self.hey(str)
+    # remove all whitespace
+    str.gsub!(/\s/, "")
+    
+    reply = case
+      # this case must go before the str.upcase check, str.upcase on "" returns true. 
+      when str == ""
+        "Fine. Be that way!"
+      # TODO: find solution that is not true when str == ""
+      when str == str.upcase && !str.match(/^[^a-zA-Z]+$/)
+        "Whoa, chill out!"
+      when str[-1] == '?'
+        "Sure."
+      else
+        "Whatever."
+    end
+    reply
+  end
+end
