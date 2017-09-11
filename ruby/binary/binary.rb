914d286a90156ae74f4ceb72d08a40d703aae78a
Nick Rodriguez <endotnick@gmail.com>
2017-09-11 21:23:27 +0000

    add binary.rb

---
+++ ruby/binary/binary.rb
@@ -0,0 +1,17 @@
+module BookKeeping
+  VERSION = 3
+end
+
+class Binary
+  def self.to_decimal num
+    raise ArgumentError if num.chars.any? { |i| !("0..1").include? i}
+    count = num.length - 1
+    sum = 0
+    
+    num.chars.each do |i| 
+      sum += i.to_i * 2 ** count
+      count -= 1
+    end
+    sum
+  end
+end
