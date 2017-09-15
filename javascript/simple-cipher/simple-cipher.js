e1d79547a637461db14e55e16236fb2ac4b96e60
Nick Rodriguez <endotnick@gmail.com>
2017-09-15 06:39:47 +0000

    14/15 tests passing

--- javascript/simple-cipher/simple-cipher.js
+++ javascript/simple-cipher/simple-cipher.js
@@ -1,28 +1,48 @@
+var alpha = 'abcdefghijklmnopqrstuvwxyz';
+
 function Cipher(key) { 
-  console.log("'" + key + "'");
   if (key != undefined && !key.match(/^[a-z]+$/)){
     throw Error('Bad key');
   }
-  
   this.key = key || generateKey();
-
-
-
-  
 }
 
-Cipher.prototype.encode = function(plainText){ 
-  return plainText;
+Cipher.prototype.encode = function(clearText){ 
+  var cipherText = "";
+  var self = this;
+  
+  clearText.split('').forEach(function(char, index) {
+      var shift = alpha.indexOf(char) + alpha.indexOf(self.key[index]);
+      if (shift >= alpha.length) {
+        shift -= alpha.length;
+      }
+      cipherText += alpha[shift];
+  });
+  return cipherText;
 };
 
-Cipher.prototype.decode = function(cipher){ 
-  return cipher;
+Cipher.prototype.decode = function(cipherText){ 
+  var clearText = "";
+  var self = this;
+  
+  cipherText.split('').forEach(function(char, index) {
+      var shift = alpha.indexOf(char) - alpha.indexOf(self.key[index]);
+      if (shift < 0) {
+        shift += alpha.length;
+      }
+      clearText += alpha[shift];
+  });
+  return clearText;
 };
 
 function generateKey() {
-
+  var randomKey = '';
+  
+	for (var i = 0; i < 100; i++) {
+		var randIndex = Math.floor(Math.random() * alpha.length);
+		randomKey += alpha[randIndex];
+	}
+	return randomKey;
 }
 
-
 module.exports = Cipher;
-
