b3af1b37206e91768b1f36d292c667ccf5b25da8
Nick Rodriguez <endotnick@gmail.com>
2017-09-16 21:38:00 +0000

    complete simple cipher - 15/15 tests passing

--- javascript/simple-cipher/simple-cipher.js
+++ javascript/simple-cipher/simple-cipher.js
@@ -7,42 +7,47 @@ function Cipher(key) {
   this.key = key || generateKey();
 }
 
-Cipher.prototype.encode = function(clearText){ 
-  var cipherText = "";
-  var self = this;
-  
-  clearText.split('').forEach(function(char, index) {
-      var shift = alpha.indexOf(char) + alpha.indexOf(self.key[index]);
-      if (shift >= alpha.length) {
-        shift -= alpha.length;
-      }
-      cipherText += alpha[shift];
-  });
+Cipher.prototype.encode = function(clearText){
+  var cipherText = this.generateOutput(clearText, true);
   return cipherText;
 };
 
 Cipher.prototype.decode = function(cipherText){ 
-  var clearText = "";
-  var self = this;
-  
-  cipherText.split('').forEach(function(char, index) {
-      var shift = alpha.indexOf(char) - alpha.indexOf(self.key[index]);
-      if (shift < 0) {
-        shift += alpha.length;
-      }
-      clearText += alpha[shift];
-  });
+  var clearText = this.generateOutput(cipherText, false);
   return clearText;
+};
+
+// given a string, encodes or decodes the string based on the is_encoding bit
+Cipher.prototype.generateOutput = function(input, is_encoding){
+  var output = "";
+  var shift, charCode, val;
+  for (var i = 0; i < input.length; i++) {
+    shift = alpha.indexOf(this.key[i % this.key.length]);
+    val = (is_encoding == true) ? (input[i].charCodeAt() + shift) : (input[i].charCodeAt() - shift);
+    charCode = wrap(val);
+    output += String.fromCharCode(charCode);
+  }
+  return output;
 };
 
 function generateKey() {
   var randomKey = '';
-  
 	for (var i = 0; i < 100; i++) {
-		var randIndex = Math.floor(Math.random() * alpha.length);
-		randomKey += alpha[randIndex];
+		randomKey += alpha[Math.floor(Math.random() * alpha.length)];
 	}
 	return randomKey;
+}
+
+// if char is outside the range 'a'..'z', wraps the char back into the range.
+function wrap(charCode){
+  if (charCode > 122) {
+    return charCode - 26;
+  } 
+  else if (charCode < 97) {
+    return charCode + 26;
+  } else {
+    return charCode;
+  }
 }
 
 module.exports = Cipher;
