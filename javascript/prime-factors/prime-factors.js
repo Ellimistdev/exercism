ebb010538207c41d965c688705915bdf8ca556af
Nick Rodriguez <endotnick@gmail.com>
2017-10-01 00:55:27 +0000

    lint prime-factors.js

--- javascript/prime-factors/prime-factors.js
+++ javascript/prime-factors/prime-factors.js
@@ -1,18 +1,18 @@
 function PrimeFactors() {}
 
-PrimeFactors.prototype.for = function(num){
-  var testNum = num;
-  var divisor = 2;
-  var factors = [];
-  
-  while (testNum > 1){
-    if (testNum % divisor === 0){
-      testNum = testNum / divisor;
-      factors.push(divisor);  
+PrimeFactors.prototype.for = function(num) {
+  let testNum = num;
+  let divisor = 2;
+  const factors = [];
+
+  while (testNum > 1) {
+    if (testNum % divisor === 0) {
+      testNum /= divisor;
+      factors.push(divisor);
       // retry successful factor
-      divisor--;
+      divisor -= 1;
     }
-  divisor++;
+    divisor += 1;
   }
   return factors;
 };
