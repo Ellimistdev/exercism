daca02bc0de1dcf7374ad2cc673452aa4aae1cb2
Nick Rodriguez <endotnick@gmail.com>
2017-10-05 14:48:38 +0000

    add phone-number.rb

---
+++ ruby/phone-number/phone_number.rb
@@ -0,0 +1,11 @@
+class PhoneNumber
+  def self.clean(num)
+    phone = num.gsub(/\D/, '').gsub(/^1/, '')
+    return nil if (phone.length != 10) || !phone.match(/^[2-9]..[2-9]/)
+    phone
+  end
+end
+
+module BookKeeping
+  VERSION = 2
+end
