4cdea862490dcfe8bffe9dc5553f064af5f91315
Nick Rodriguez <endotnick@gmail.com>
2017-10-16 17:17:05 +0000

    add anagram.js

---
+++ javascript/anagram/anagram.js
@@ -0,0 +1,33 @@
+class Anagram {
+  constructor(needle) {
+    this.needle = needle.toLowerCase();
+  }
+
+  matches(...args) {
+    // given [] or series of strings, use an []
+    const haystack = args[0] instanceof Array ? args[0] : args;
+    const match = haystack.filter(entry => this.compare(entry, this.needle));
+    // remove entries that match needle
+    return match.filter(entry => entry.toLowerCase() !== this.needle);
+  }
+
+  compare(entry, target) {
+    this.target = target.split('').sort();
+    this.entry = entry.toLowerCase().split('').sort();
+
+    // bail if lengths are !==
+    if (this.entry.length !== this.target.length) {
+      return false;
+    }
+
+    // compare each char
+    for (let i = 0; i < this.entry.length; i += 1) {
+      if (this.entry[i] !== this.target[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+module.exports = Anagram;
