d628b3deb05203a1787ef9ec6cfcabc7cf7ea0d7
Nick Rodriguez <endotnick@gmail.com>
2017-10-23 20:25:11 +0000

    add Allergies.cs

---
+++ csharp/allergies/Allergies.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+
+public class Allergies
+{
+    private List<string> allergies = new List<string>();
+    private Dictionary<byte, string> _allergies = new Dictionary<byte, string>
+    {
+        {0b0000_0001, "eggs"},
+        {0b0000_0010, "peanuts"},
+        {0b0000_0100, "shellfish"},
+        {0b0000_1000, "strawberries"},
+        {0b0001_0000, "tomatoes"},
+        {0b0010_0000, "chocolate"},
+        {0b0100_0000, "pollen"},
+        {0b1000_0000, "cats"}
+    };
+    
+    public Allergies(int mask)
+    {
+        byte allergySeed = (byte)mask;
+        
+        foreach (byte allergy in _allergies.Keys)
+        {
+            if ((allergySeed & allergy) == allergy)
+            {
+                allergies.Add(_allergies[allergy]);
+            }
+        }
+    }
+
+    public bool IsAllergicTo(string allergy)
+    {
+        return allergies.Contains(allergy);
+    }
+
+    public IList<string> List()
+    {
+        return allergies;
+    }
+}
