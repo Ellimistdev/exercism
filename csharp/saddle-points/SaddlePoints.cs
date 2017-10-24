f1be1eda979bf4cdffed9089ea9c4502e5aef6bb
Nick Rodriguez <endotnick@gmail.com>
2017-10-24 19:16:07 +0000

    extract testSet generation

--- csharp/saddle-points/SaddlePoints.cs
+++ csharp/saddle-points/SaddlePoints.cs
@@ -5,50 +5,49 @@ using System.Linq;
 public class SaddlePoints
 {
     private int[,] matrix;
+    private int matrixD0;
+    private int matrixD1;
     public SaddlePoints(int[,] values)
     {
         matrix = values;
+        matrixD0 = matrix.GetLength(0);
+        matrixD1 = matrix.GetLength(1);
     }
 
     public IEnumerable<Tuple<int, int>> Calculate()
     {
-        return GetSaddlePoints(GetRowMins());
-    }
-    
-    private List<Tuple<int, int, int>> GetRowMins()
-    {
-        // rowIndex, columnIndex, value
-        List<Tuple<int, int, int>> testPoints = new List<Tuple<int, int, int>> (); 
-        for (int r = 0; r < matrix.GetLength(0); r++)
+        var testPoints = new List<Tuple<int, int>> (); 
+        var saddlePoints = new List<Tuple<int, int>> (); 
+        
+        for (var r = 0; r < matrixD0; r++)
         {
-            int[] row = new int[matrix.GetLength(1)];
-            for (int c = 0; c < matrix.GetLength(1); c++)
-            {
-                row[c] = matrix[r,c];
-            }
-            var max = row.Max();
-            testPoints.Add(new Tuple<int, int, int>(r, Array.IndexOf(row, max), max));
+            var row = TestSet(matrixD1, r, true);
+            
+            testPoints.Add(Tuple.Create(r, Array.IndexOf(row, row.Max())));
         }
-        return testPoints;
-    }
-    
-    private Tuple<int,int>[] GetSaddlePoints(List<Tuple<int, int, int>> testPoints)
-    {
-        // rowIndex, columnIndex,
-        List<Tuple<int, int>> saddlePoints = new List<Tuple<int, int>> (); 
+        
         foreach (var testPoint in testPoints)
         {
-            int[] testColumn = new int[matrix.GetLength(1)];
-            for (int r = 0; r < matrix.GetLength(1); r++)
-            {
-                testColumn[r] = matrix[r,testPoint.Item2];    
-            }
+            var testColumn = TestSet(matrixD0, testPoint.Item2, false);
             
-            if (testPoint.Item3 <= testColumn.Min())
+            if (matrix[testPoint.Item1, testPoint.Item2] <= testColumn.Min())
             {
-                saddlePoints.Add(new Tuple<int,int>(testPoint.Item1, testPoint.Item2));
+                saddlePoints.Add(Tuple.Create(testPoint.Item1, testPoint.Item2));
             }
         }
+        
         return saddlePoints.ToArray();
+    }
+    
+    private int[] TestSet(int size, int x, bool isRow)
+    {
+        var set = new int[size];
+            
+            for (int i = 0; i < size; i++)
+            {
+                set[i] = isRow ? matrix[x,i] : matrix[i,x];
+            }
+            
+        return set;
     }
 }
