05233bc7428f491ac25369bc2c779107502140d8
Nick Rodriguez <endotnick@gmail.com>
2017-10-25 15:07:19 +0000

    complete markdown.cs first pass

--- csharp/markdown/Markdown.cs
+++ csharp/markdown/Markdown.cs
@@ -4,106 +4,84 @@ using System.Text.RegularExpressions;
 // Ordered methods in the sequence they are called.
 public static class Markdown
 {
-    private static bool _list; // extract list check that's being passed around
+    private static int _liCount; // extract list check that's being passed around
+    private static int _liMax;
     public static string Parse(string markdown)
     {
         var lines = markdown.Split('\n');
         var result = "";
-        _list = false;
-        
+        _liCount = 0;
+        _liMax = 0;
+
+        foreach (var line in lines)
+        {
+            if (line.StartsWith("*"))
+            {
+                _liMax++;
+                _liCount++;
+            }
+        }
+
         for (int i = 0; i < lines.Length; i++)
         {
             result += ParseLine(lines[i]);  // no need to declare var lineresult
         }
-        
-        return _list ? result + "</ul>" : result; // switched to ternary
+
+        return result; // moved <ul> tag assignment to ParseLineItem()
     }
-    
-    // combine null checks & assignment
+
+    // combine null checks
     private static string ParseLine(string markdown)
     {
-        return ParseHeader(markdown) ?? 
-               ParseLineItem(markdown) ?? 
-               ParseParagraph(markdown) ??
-               throw new ArgumentException("Invalid markdown"); 
+        return ParseHeader(markdown) ??
+               ParseLineItem(markdown) ??
+               ParseText(markdown) ??
+               throw new ArgumentException("Invalid markdown"); //Replaced ParseParagraph() with ParseText
     }
-    
+
     private static string ParseHeader(string markdown)
     {
-        var regex = new Regex(@"#+");
-        var count = regex.Match(markdown).Length; // get '#' count via regex
-        
-        if (count == 0)
-        {
-            return null;
-        }
+        var count = Regex.Match(markdown, @"#+").Length; // get '#' count via regex
 
-        var headerTag = "h" + count;
-        var headerHtml = Wrap(markdown.Substring(count + 1), headerTag);
-
-        if (_list)
-        {
-            _list = false;
-            return "</ul>" + headerHtml;
-        }
-        else
-        {
-            return headerHtml;
-        }
+        return count == 0 ? null : Wrap(markdown.Substring(count + 1), "h" + count); // switched to ternary
     }
-    
+
     private static string Wrap(string text, string tag) => "<" + tag + ">" + text + "</" + tag + ">";
 
     private static string ParseLineItem(string markdown)
     {
-        var tempList = _list;
         if (markdown.StartsWith("*"))
         {
-            _list = true;
-            var innerHtml = Wrap(ParseText(markdown.Substring(2)), "li");
-            _list = tempList;
-            
-            if (_list)
-            {
-                return innerHtml;
-            }
-            else
-            {
-                _list = true;
+            var innerHtml = Wrap(ParseText(markdown.Substring(2), true), "li");
+            _liCount--;
+
+            if(_liCount == _liMax - 1)
                 return "<ul>" + innerHtml;
-            }
+            if(_liCount > 0)
+                return innerHtml;
+
+            return innerHtml + "</ul>";
         }
 
         return null;
     }
 
-    private static string ParseText(string markdown)
+    private static string ParseText(string markdown, bool list = false)
     {
         var parsedText = Parse_(Parse__((markdown)));
 
-        return _list ? parsedText : Wrap(parsedText, "p"); // switched to ternary
+        return list ? parsedText : Wrap(parsedText, "p"); // switched to ternary
     }
 
     private static string Parse__(string markdown) => Parse(markdown, "__", "strong");
     private static string Parse_(string markdown) => Parse(markdown, "_", "em");
-    
+
     private static string Parse(string markdown, string delimiter, string tag)
     {
         var pattern = delimiter + "(.+)" + delimiter;
         var replacement = "<" + tag + ">$1</" + tag + ">";
+
         return Regex.Replace(markdown, pattern, replacement);
     }
-    
-    private static string ParseParagraph(string markdown)
-    {
-        if (!_list)
-        {
-            return ParseText(markdown);
-        }
-        else
-        {
-            _list = false;
-            return "</ul>" + ParseText(markdown);
-        }
-    }
+    // removed unused methods ParseParagraph() and IsTag()
 }
