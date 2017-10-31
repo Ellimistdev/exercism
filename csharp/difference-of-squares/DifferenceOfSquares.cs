e2f58b95e8f725b51046d68f8c1fd1c22b761785
Nick Rodriguez <endotnick@gmail.com>
2017-10-31 17:27:31 +0000

    extract range method

--- csharp/difference-of-squares/DifferenceOfSquares.cs
+++ csharp/difference-of-squares/DifferenceOfSquares.cs
@@ -1,19 +1,28 @@
 using System;
 using System.Linq;
+using System.Collections.Generic;
 
 public static class DifferenceOfSquares
 {
-    public static int CalculateSquareOfSum(int max) =>
-        Enumerable.Range(1, max).Sum().Square();
+    public static int CalculateSquareOfSum(int max) => max
+        .ToNaturalRange()
+        .Sum()
+        .Square();
 
-    public static int CalculateSumOfSquares(int max) =>
-        Enumerable.Range(1, max).Select(IntExtensions.Square).Sum();
+    public static int CalculateSumOfSquares(int max) => max
+        .ToNaturalRange()
+        .Select(IntExtensions.Square)
+        .Sum();
 
-    public static int CalculateDifferenceOfSquares(int max) =>
-        CalculateSquareOfSum(max) - CalculateSumOfSquares(max);
+    public static int CalculateDifferenceOfSquares(int max) 
+        => CalculateSquareOfSum(max) - CalculateSumOfSquares(max);
 }
 
 public static class IntExtensions
 {
-    public static int Square(this int n) => n * n;
+    public static IEnumerable<int> ToNaturalRange(this int max)
+        => Enumerable.Range(1, max);
+        
+    public static int Square(this int n) 
+        => n * n;
 }
