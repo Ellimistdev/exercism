aac0463d3c50aeaff2fda91793e4d92a6aa088f1
Nick Rodriguez <endotnick@gmail.com>
2017-10-31 19:06:24 +0000

    improve readability

--- csharp/pangram/Pangram.cs
+++ csharp/pangram/Pangram.cs
@@ -3,6 +3,7 @@ using System.Text.RegularExpressions;
 
 public static class Pangram
 {
-    public static bool IsPangram(string input)
-        => Regex.Matches(input.ToLower(), @"([a-z])(?!.*\1)").Count == 26;
+    public static bool IsPangram(string input) => Regex
+        .Matches(input.ToLower(), @"([a-z])(?!.*\1)")
+        .Count == 26;
 }
