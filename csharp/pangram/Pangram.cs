22f6663bbf36c2c9fdf464e0d4f47c47f026d0e2
Nick Rodriguez <endotnick@gmail.com>
2017-10-31 19:04:20 +0000

    add Pangram.cs

---
+++ csharp/pangram/Pangram.cs
@@ -0,0 +1,8 @@
+using System;
+using System.Text.RegularExpressions;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input)
+        => Regex.Matches(input.ToLower(), @"([a-z])(?!.*\1)").Count == 26;
+}
