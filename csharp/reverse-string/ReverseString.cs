c50c248ca1db8a4d4779cc2da8d1c43a80e837aa
Nick Rodriguez <endotnick@gmail.com>
2018-01-02 18:03:39 +0000

    clean ReverseString.cs

--- csharp/reverse-string/ReverseString.cs
+++ csharp/reverse-string/ReverseString.cs
@@ -3,8 +3,6 @@ using System.Linq;
 
 public static class ReverseString
 {
-    public static string Reverse(string input)
-    {
-        return new string(input.ToCharArray().Reverse().ToArray());
-    }
+    public static string Reverse(string input) =>
+        new string(input.ToCharArray().Reverse().ToArray());
 }
